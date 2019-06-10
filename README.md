570e07d7f713901a74f326f25baaaf3e82b9872f
erirore <eeseinone@gmail.com>
2019-06-10 21:03:24 +0100

    added file

--- README.md
+++
@@ -1,1 +0,0 @@
-# erirore.github.io
---
+++ bold.js
@@ -0,0 +1,36 @@
+function imageChange()
+{
+  document.getElementById("bold").style.width="500px";
+  document.getElementById("bold").style.height="500px";
+   document.getElementById("peac2").style.fontWeight="bold";
+   document.getElementById("peac2").style.fontSize="40px";
+ return true;
+ }
+ 
+ function changeBack()
+{
+  document.getElementById("bold").style.width="280px";
+  document.getElementById("bold").style.height="270px";
+  return true;
+ }
+ function imageWhite()
+   {
+    document.getElementById("culv").style.width="600px";
+	document.getElementById("culv").style.height="800px";
+	return true;
+	}
+	 function changeBk2()
+{
+  document.getElementById("culv").style.width="280px";
+  document.getElementById("culv").style.height="270px";
+  return true;
+ }
+function fast()
+ {
+document.getElementById("marquee2").style.fontSize="300px";
+return true;
+}
+
+ 
+ 
+	
